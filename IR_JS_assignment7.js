9202947a024195315c687d52a4ca7a5a53fb65a0
IRathoor <i.rathoor@gmail.com>
2023-09-12 21:47:17 +0500

    Initial

---
+++ IR_JS_assignment7.js
@@ -0,0 +1,8 @@
+//Q21
+var university = ['University of Karachi']
+for(var i = 0; i<university.length; i++){
+    var nestedArray= university[i]
+    for (var j = 0; j <nestedArray.length; j++){
+    console.log(nestedArray[j])
+        }
+}
